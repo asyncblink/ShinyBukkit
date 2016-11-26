aee7c314a2ab909c2c50fded1149cbfb589e6681
asyncblink <asyncblink@protonmail.com>
2016-11-26 19:15:46 +1300

    Initial Commit

---
+++ version.sh
@@ -0,0 +1,5 @@
+NMS_VERSION="1.11"
+NMS_JAR="cache/nms_${NMS_VERSION}.jar"
+NMS_URL="https://s3.amazonaws.com/Minecraft.Download/versions/${NMS_VERSION}/minecraft_server.${NMS_VERSION}.jar"
+NMS_MD5=77af7daf2793bf4be81edb623db7cbf3
+
